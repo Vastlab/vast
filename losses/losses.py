d0db1a65ddd45baaa31cdc2458218dbf6ee1f1f8
akshay <adhamija@uccs.edu>
2020-03-12 16:01:53 -0600

    objectosphere+centerLoss

--- losses/losses.py
+++ losses/losses.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import functional as F
 
 '''
 use "update_centers(.)" after optimizer.step() in dataloader loop
@@ -40,6 +41,43 @@ class center_loss:
                                                                      fc.device))
         return torch.mean(loss)
 
+
+
+class objecto_center_loss:
+    def __init__(self, alpha=0.1, classes=range(10), fc_layer_dimension=2, ring_size=50):
+        """
+        :param alpha:  The factor by which learning rate should be updated
+        :param classes: A list containing class labels for which we will be computing center loss \
+                        Note-> This list should only contain positive numbers, negatives are reserved for unknowns
+        :param fc_layer_dimension: The dimension of the layer in which center loss is being computed
+        """
+        self.alpha = alpha
+        self.ring_size = ring_size
+        self.centers = dict(zip(classes,
+                                torch.zeros((len(classes), fc_layer_dimension)).requires_grad_(requires_grad=False)
+                            ))
+        self.euclidean_dist_obj = torch.nn.PairwiseDistance(p=2)
+
+    def update_centers(self, fc_outputs, true_label):
+        # find all samples of knowns
+        for cls_no in set(true_label[true_label >= 0].tolist()):
+            # Equation (4) from the paper
+            delta_c = fc_outputs[true_label == cls_no].cpu().detach() - self.centers[cls_no]
+            delta_c = torch.sum(delta_c, dim=0) / fc_outputs[true_label == cls_no].shape[0]
+
+            delta_c = delta_c/torch.norm(delta_c, p=2, dim=-1)
+            # Update Direction
+            self.centers[cls_no] = self.centers[cls_no] + (self.alpha * delta_c)
+            self.centers[cls_no] = self.ring_size*(self.centers[cls_no]/torch.norm(self.centers[cls_no], p=2, dim=-1))
+
+    def compute_loss(self, fc, true_label):
+        # Equation (2) from paper
+        loss = torch.zeros(fc.shape[0]).to(fc.device)
+        for cls_no in set(true_label.tolist()):
+            loss[true_label == cls_no] = self.euclidean_dist_obj(fc[true_label == cls_no],
+                                                              self.centers[cls_no].expand_as(fc[true_label == cls_no]).to(
+                                                                  fc.device))
+        return torch.mean(loss)
 
 def entropic_openset_loss(logit_values, target, num_of_classes=10, sample_weights=None):
     catagorical_targets = torch.zeros(logit_values.shape)
