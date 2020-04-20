3e3845d65e22064da12102d96d73ef300bedc2cf
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2020-04-20 09:46:04 -0600

    new implementation of objectocenter

--- losses/losses.py
+++ losses/losses.py
@@ -62,45 +62,20 @@ class tensor_center_loss:
     def __call__(self, FV, true_label):
         # Equation (2) from paper
         loss = self.euclidean_dist_obj(FV,self.centers[true_label,:].to(FV.device))
-        return torch.mean(loss)
+        return loss
 
 
 
-class objecto_center_loss:
-    def __init__(self, alpha=0.1, classes=range(10), fc_layer_dimension=2, ring_size=50):
-        """
-        :param alpha:  The factor by which learning rate should be updated
-        :param classes: A list containing class labels for which we will be computing center loss \
-                        Note-> This list should only contain positive numbers, negatives are reserved for unknowns
-        :param fc_layer_dimension: The dimension of the layer in which center loss is being computed
-        """
-        self.alpha = alpha
-        self.ring_size = ring_size
-        self.centers = dict(zip(classes,
-                                torch.zeros((len(classes), fc_layer_dimension)).requires_grad_(requires_grad=False)
-                            ))
-        self.euclidean_dist_obj = torch.nn.PairwiseDistance(p=2)
+class objecto_center_loss(tensor_center_loss):
+    def __init__(self, beta=0.1, classes=range(10), fc_layer_dimension=2, ring_size=50, initial_value=None, unknowns_label=-1):
+        super().__init__(beta=beta, classes=classes, fc_layer_dimension=fc_layer_dimension, initial_value=initial_value)
+        self.unknowns_label = unknowns_label
 
-    def update_centers(self, fc_outputs, true_label):
-        # find all samples of knowns
-        for cls_no in set(true_label[true_label >= 0].tolist()):
-            # Equation (4) from the paper
-            delta_c = fc_outputs[true_label == cls_no].cpu().detach() - self.centers[cls_no]
-            delta_c = torch.sum(delta_c, dim=0) / fc_outputs[true_label == cls_no].shape[0]
-
-            delta_c = delta_c/torch.norm(delta_c, p=2, dim=-1)
-            # Update Direction
-            self.centers[cls_no] = self.centers[cls_no] + (self.alpha * delta_c)
-            self.centers[cls_no] = self.ring_size*(self.centers[cls_no]/torch.norm(self.centers[cls_no], p=2, dim=-1))
-
-    def __call__(self, fc, true_label):
-        # Equation (2) from paper
-        loss = torch.zeros(fc.shape[0]).to(fc.device)
-        for cls_no in set(true_label.tolist()):
-            loss[true_label == cls_no] = self.euclidean_dist_obj(fc[true_label == cls_no],
-                                                              self.centers[cls_no].expand_as(fc[true_label == cls_no]).to(
-                                                                  fc.device))
-        return torch.mean(loss)
+    def update_centers(self, FV, true_label):
+        FV = FV.detach()
+        deltas = FV - self.centers[true_label, :].requires_grad_(requires_grad=False)
+        for cls_no in set(true_label.tolist())-set([self.unknowns_label]):
+            self.centers[cls_no] += (self.beta * torch.mean(deltas[true_label == cls_no], dim=0))
 
 class entropic_openset_loss():
     def __init__(self, num_of_classes=10):
@@ -112,9 +87,9 @@ class entropic_openset_loss():
     def __call__(self, logit_values, target, sample_weights=None):
         catagorical_targets = torch.zeros(logit_values.shape).cuda()
         known_indexes = target != -1
-        unknown_indexes = target == -1
+        unknown_indexes = ~known_indexes
         catagorical_targets[known_indexes, :] = self.eye[target[known_indexes]]
-        catagorical_targets[unknown_indexes, :] = self.ones.expand((target[unknown_indexes].shape[0],self.num_of_classes)) * self.unknowns_multiplier
+        catagorical_targets[unknown_indexes, :] = self.ones.expand((torch.sum(unknown_indexes).item(),self.num_of_classes)) * self.unknowns_multiplier
         log_values = F.log_softmax(logit_values, dim=1)
         negative_log_values = -1 * log_values
         loss = negative_log_values * catagorical_targets
@@ -142,7 +117,7 @@ class objectoSphere_loss():
         mag_diff_from_ring = torch.clamp(self.knownsMinimumMag_tensor - mag, min=0.)
         loss = self.zeros
         known_indexes = target != -1
-        unknown_indexes = target == -1
+        unknown_indexes = ~known_indexes
         loss[known_indexes] = mag_diff_from_ring[known_indexes]
         loss[unknown_indexes] = mag[unknown_indexes]
         loss = torch.pow(loss, 2)
