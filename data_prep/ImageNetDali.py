620dfe465ae085cc30c7aede17b220dde85e1c68
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2020-04-21 22:16:45 -0600

    Approach Didn't Work

--- data_prep/ImageNetDali.py
+++ data_prep/ImageNetDali.py
@@ -12,8 +12,9 @@ import nvidia.dali.types as nvidia_types
 # Only needed for backward compatibility: https://stackoverflow.com/questions/4015417/python-class-inherits-object
 class ExternalInputIterator():
     def __init__(self, batch_size, device_id, num_gpus, filenames, images_path,
-                 debug=False, random_seed=None, unknowns_label=-1):
+                 debug=False, random_seed=None, unknowns_label=-1, return_file_names=False):
         self.batch_size = batch_size
+        self.return_file_names = return_file_names
         self.files = []
         for file_no,filename in enumerate(filenames):
             if filename.suffix == '.csv':
@@ -49,6 +50,7 @@ class ExternalInputIterator():
     def __next__(self):
         batch_inputs = []
         batch_labels = []
+        if self.return_file_names: batch_file_names = []
         if self.i >= self.n:
             raise StopIteration
         while len(batch_inputs)<self.batch_size and self.i<self.n:
@@ -56,6 +58,11 @@ class ExternalInputIterator():
             f = open(self.images_path+jpeg_path, 'rb')
             batch_inputs.append(np.frombuffer(f.read(), dtype=np.uint8))
             batch_labels.append(np.array([label], dtype=np.uint8))
+            if self.return_file_names:
+                name = jpeg_path.split('/')[-1]
+                name = int(name.split('.')[0].split('_')[-1])
+                batch_file_names.append(np.array([name]).astype(np.int32))
+#                print ("batch_file_names",batch_file_names)
             self.i += 1
         if self.i==self.n:
             counter = 0
@@ -64,7 +71,10 @@ class ExternalInputIterator():
                 f = open(self.images_path + jpeg_path, 'rb')
                 batch_inputs.append(np.frombuffer(f.read(), dtype=np.uint8))
                 batch_labels.append(np.array([label], dtype=np.uint8))
+                if self.return_file_names: batch_file_names.append(np.array([-1]).astype(np.int32))
                 counter+=1
+        if self.return_file_names:
+            return (batch_inputs, batch_labels, batch_file_names)
         return (batch_inputs, batch_labels)
 
     @property
@@ -73,21 +83,27 @@ class ExternalInputIterator():
     next = __next__
 
 class ExternalSourcePipeline(Pipeline):
-    def __init__(self, batch_size, num_threads, device_id, external_data, device_type="gpu", training=False):
+    def __init__(self, batch_size, num_threads, device_id, external_data, device_type="gpu",
+                 training=False, image_size=224, return_file_names=False):
         super(ExternalSourcePipeline, self).__init__(batch_size, num_threads, device_id, seed=34,prefetch_queue_depth={ "cpu_size": 10, "gpu_size": 2})
         self.input = nvidia_ops.ExternalSource()
         self.input_label = nvidia_ops.ExternalSource()
+        self.return_file_names = return_file_names
+        if self.return_file_names:
+            self.input_filenames = nvidia_ops.ExternalSource()
         self.training = training
         self.crop_loc = nvidia_ops.Uniform(range=(0.,1.))
         self.coin = nvidia_ops.CoinFlip(probability=0.5)
         self.rotation_angle = nvidia_ops.Uniform(range=(0.,10.))
         self.decode = nvidia_ops.ImageDecoder(device="mixed" if device_type=="gpu" else "cpu", output_type=nvidia_types.RGB)
         self.rotate = nvidia_ops.Rotate(device=device_type)
-        self.resize = nvidia_ops.Resize(device=device_type, resize_shorter=256)
-        self.crop_mirror_normalize = nvidia_ops.CropMirrorNormalize(device=device_type, crop=(224,224),mean=128,std=128,output_layout='HWC')
+        self.resize = nvidia_ops.Resize(device=device_type, resize_shorter=(image_size*256/224))
+        self.crop_mirror_normalize = nvidia_ops.CropMirrorNormalize(device=device_type, crop=(image_size,image_size),
+                                                                    mean=128,std=128,output_layout='HWC')
         self.jitter = nvidia_ops.Jitter(device="gpu", nDegree=4)
         self.transpose = nvidia_ops.Transpose(device="gpu",perm=(2,0,1))
         self.cast = nvidia_ops.Cast(device="gpu", dtype=nvidia_types.FLOAT)
+        # self.file_names_cast = nvidia_ops.Cast(device="cpu", dtype=nvidia_types.STRING)
         self.external_data = external_data
         self.iterator = iter(self.external_data)
         self.device_type = device_type
@@ -113,6 +129,8 @@ class ExternalSourcePipeline(Pipeline):
     def define_graph(self):
         self.jpegs = self.input()
         self.labels = self.input_label()
+        if self.return_file_names:
+            self.file_names = self.input_filenames()
         images = self.decode(self.jpegs)
         if self.training:
             images = self.training_data_augmentation(images)
@@ -120,13 +138,22 @@ class ExternalSourcePipeline(Pipeline):
             images = self.validation_data_augmentation(images)
         images = self.transpose(images)
         output = self.cast(images)
+        if self.return_file_names:
+            return (output, self.labels, self.file_names)
+        # return (output, self.labels, self.file_names_cast(self.file_names))
         return (output, self.labels)
 
     def iter_setup(self):
         try:
-            (images, labels) = self.iterator.next()
-            self.feed_input(self.jpegs, images)
-            self.feed_input(self.labels, labels)
+            if self.return_file_names:
+                (images, labels, file_names) = self.iterator.next()
+                self.feed_input(self.jpegs, images)
+                self.feed_input(self.labels, labels)
+                self.feed_input(self.file_names, file_names)
+            else:
+                (images, labels) = self.iterator.next()
+                self.feed_input(self.jpegs, images)
+                self.feed_input(self.labels, labels)
         except StopIteration:
             self.iterator = iter(self.external_data)
             raise StopIteration
