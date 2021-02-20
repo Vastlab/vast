928138eff838d98ae597ab150776bb0e9ed41d2d
tboult <tboult@vast.uccs.edu>
2021-02-19 19:45:54 -0700

    TB adds FitHighFlipped to Weibull

--- DistributionModels/weibull.py
+++ DistributionModels/weibull.py
@@ -40,6 +40,26 @@ class weibull:
         return to_return
 
 
+    # new mode that flips the data around the max and then does a fit low reject so that its effectively modeling just above the max . 
+    def FitHighFlipped(self,data, tailSize, isSorted=False, gpu=0):
+        """
+        data --> 5000 weibulls on 0 dim
+             --> 10000 distances for each weibull on 1 dim
+        """
+        self.sign = -1
+        maxval = data.max()
+        #flip the data around 
+        data = 2* maxval - data
+        max_tailsize_in_1_chunk = 100000
+        if tailSize <= max_tailsize_in_1_chunk:
+            self.splits = 1
+            to_return = self._weibullFitting(data, tailSize, isSorted, gpu)
+        else:
+            self.splits = tailSize//max_tailsize_in_1_chunk + 1
+            to_return = self._weibullFilltingInBatches(data, tailSize, isSorted, gpu)
+        return to_return
+
+
     def FitHigh(self, data, tailSize, isSorted=False):
         self.sign = 1
         self.splits = 1
