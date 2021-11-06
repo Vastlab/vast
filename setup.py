d99bb617effd0e6f5cdb95b32ef0c295ba4585aa
Akshay Raj Dhamija <akshay.raj.dhamija@gmail.com>
2021-11-06 12:04:04 -0600

    Fixes to development install

--- setup.py
+++ setup.py
@@ -4,22 +4,28 @@ import shlex
 import setuptools
 from setuptools import setup
 from setuptools.command.develop import develop
-from setuptools.command.install import install
 from subprocess import check_call
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
+with open("dev-requirements.txt", "r", encoding="utf-8") as fh:
+    dev_requirements = fh.read()
 
 
-class PostInstallCommand(install):
+class PostDevelopCommand(develop):
     def run(self):
         try:
             check_call(shlex.split("pre-commit install"))
-        except Exception:
-            print("\n\n\nUnable to run 'pre-commit install'\n\n\n")
-        install.run(self)
+        except Exception as err:
+            print(
+                f"\n\n\n"
+                f"Unable to run 'pre-commit install'\n"
+                f"Ignore this message if you do not intend to make pull requests\n"
+                f"{err}\n\n\n"
+            )
+        develop.run(self)
 
 
 setup(
@@ -30,9 +36,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
+    extras_require={"dev": dev_requirements},
     packages=setuptools.find_packages(),
     url="https://github.com/Vastlab/vast",
-    cmdclass={
-        "install": PostInstallCommand,
-    },
+    cmdclass={"develop": PostDevelopCommand},
 )
