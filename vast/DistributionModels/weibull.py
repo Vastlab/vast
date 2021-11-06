b5967596021c69bbc1c631b8cc1fc860671d8680
Akshay Raj Dhamija <akshay.raj.dhamija@gmail.com>
2021-11-06 13:52:59 -0600

    Adding changes from PR#16

--- vast/DistributionModels/weibull.py
+++ vast/DistributionModels/weibull.py
@@ -2,6 +2,7 @@
 Author: Terrance E. Boult.
 This class extends the default libmr and aims to provide more useful curve fitting options.
 """
+import torch
 from .libmr import libmr
 
 
@@ -24,3 +25,18 @@ class weibull(libmr):
             self.splits = tailSize // max_tailsize_in_1_chunk + 1
             to_return = self._weibullFilltingInBatches(data, tailSize, isSorted, gpu)
         return to_return
+
+    def FitLowReversed(self, data, tailSize, isSorted=False, gpu=0):
+        self.reversed = True
+        return self.FitLow(self, data, tailSize, isSorted=False, gpu=0)
+
+    def prob(self, distances):
+        """
+        This function can calculate raw probability scores from various weibulls for a given set of distances
+        :param distances: a 2-D tensor with the number of rows equal to number of samples and number of columns equal to number of weibulls
+        Or
+        a 1-D tensor with number of elements equal to number of test samples
+        :return:
+        """
+        weibulls, distances = self.compute_weibull_object(distances)
+        return torch.exp(weibulls.log_prob(distances))
