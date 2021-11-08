8e19778b689c72d01b4b66bff36a27370e4508f1
Akshay Raj Dhamija <akshay.raj.dhamija@gmail.com>
2021-11-08 12:27:53 -0700

    Removing redundant code in FitHighFlipped and a bug fix from previous commit

--- vast/DistributionModels/weibull.py
+++ vast/DistributionModels/weibull.py
@@ -9,26 +9,14 @@ from .libmr import libmr
 class weibull(libmr):
     # TB's new mode that flips the data around the max and then does a fit low reject so that its effectively modeling just above the max .
     def FitHighFlipped(self, data, tailSize, isSorted=False, gpu=0):
-        """
-        data --> 5000 weibulls on 0 dim
-             --> 10000 distances for each weibull on 1 dim
-        """
-        self.sign = -1
         maxval = data.max()
         # flip the data around the max so the smallest points are just beyond the data but mirrors the distribution
         data = 2 * maxval - data
-        max_tailsize_in_1_chunk = 100000
-        if tailSize <= max_tailsize_in_1_chunk:
-            self.splits = 1
-            to_return = self._weibullFitting(data, tailSize, isSorted, gpu)
-        else:
-            self.splits = tailSize // max_tailsize_in_1_chunk + 1
-            to_return = self._weibullFilltingInBatches(data, tailSize, isSorted, gpu)
-        return to_return
+        return self.FitLow(self, data, tailSize, isSorted=isSorted, gpu=gpu)
 
     def FitLowReversed(self, data, tailSize, isSorted=False, gpu=0):
         self.reversed = True
-        return self.FitLow(self, data, tailSize, isSorted=False, gpu=0)
+        return self.FitLow(self, data, tailSize, isSorted=isSorted, gpu=gpu)
 
     def prob(self, distances):
         """
