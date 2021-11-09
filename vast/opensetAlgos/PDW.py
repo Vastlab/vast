ab9adb7522e5cd34c50d6904125da76389c3fb83
Akshay Raj Dhamija <akshay.raj.dhamija@gmail.com>
2021-11-08 19:33:28 -0700

    Adding some parameters for other OOD approaches

--- vast/opensetAlgos/PDW.py
+++ vast/opensetAlgos/PDW.py
@@ -13,20 +13,13 @@ from vast import opensetAlgos
 from vast.tools import pairwisedistances
 from vast import tools
 
+
 def PDW_Params(parser):
+    """
+    This function has been written in an untidy way, it rewrites all parameters used by the specific OOD algorithms.
+    Should be rewritten, if a better way can be found.
+    """
     PDW_params = parser.add_argument_group("PDW params")
-    PDW_params.add_argument(
-        "--set_shape_to",
-        type=float,
-        default=1.0,
-        help="Set shape to this value if none could be computed : %(default)s",
-    )
-    PDW_params.add_argument(
-        "--set_scale_to",
-        type=float,
-        default=1.0,
-        help="Set scale to this value if none could be computed : %(default)s",
-    )
     PDW_params.add_argument(
         "--tailsize",
         nargs="+",
@@ -49,17 +42,24 @@ def PDW_Params(parser):
         help="distance metric to use default: %(default)s",
     )
     PDW_params.add_argument(
+        "--Clustering_Algo",
+        default="finch",
+        type=str,
+        choices=["KMeans", "dbscan", "finch"],
+        help="Clustering algorithm used for multi modal openmax default: %(default)s",
+    )
+    PDW_params.add_argument(
+        "--cover_threshold",
+        nargs="+",
+        type=float,
+        default=[0.7],
+        help="cover threshold to use\ndefault: %(default)s",
+    )
+    PDW_params.add_argument(
         "--distances_unique",
         action="store_true",
         default=False,
         help="Use unique distances during fitting",
-    )
-    PDW_params.add_argument(
-        '--Algo_choice',
-        default='OpenMax',
-        type=str,
-        choices=['OpenMax','EVM','Turbo_EVM','MultiModalOpenMax'],
-        help='Name of the openset detection algorithm'
     )
     PDW_params.add_argument(
         "--translateAmount",
@@ -67,79 +67,93 @@ def PDW_Params(parser):
         default=1.0,
         help="translateAmount to use default: %(default)s",
     )
+    PDW_params.add_argument(
+        "--Algo_choice",
+        default="OpenMax",
+        type=str,
+        choices=["OpenMax", "EVM", "Turbo_EVM", "MultiModalOpenMax"],
+        help="Name of the openset detection algorithm",
+    )
+    PDW_params.add_argument(
+        "--set_shape_to",
+        type=float,
+        default=1.0,
+        help="Set shape to this value if none could be computed : %(default)s",
+    )
+    PDW_params.add_argument(
+        "--set_scale_to",
+        type=float,
+        default=1.0,
+        help="Set scale to this value if none could be computed : %(default)s",
+    )
     return parser, dict(
         group_parser=PDW_Params,
         param_names=("tailsize", "distance_multiplier"),
         param_id_string="TS_{}_DM_{:.2f}",
     )
 
-    """
-    if "OOD_Algo" not in known_args.__dict__ and "OOD_Algo" not in unknown_args.__dict__:
-        known_args.OOD_Algo="OpenMax"
-    else:
-        if "OOD_Algo" in known_args.__dict__:
-            known_args.OOD_Algo = known_args.OOD_Algo
-        else:
-            known_args.OOD_Algo = unknown_args.OOD_Algo
-
-    # Adding Algorithm Params
-    params_parser = argparse.ArgumentParser(parents = [PDW_params], formatter_class = argparse.RawTextHelpFormatter)
-    parser_to_return, algo_params = getattr(opensetAlgos, known_args.OOD_Algo + '_Params')(params_parser)
-
-    return parser_to_return, algo_params
-    """
 
 def PDW_Training(
     pos_classes_to_process: List[str],
     features_all_classes: Dict[str, torch.Tensor],
     args,
     gpu: int,
-    models=None
+    models=None,
 ):
     if "Algo_choice" not in args.__dict__:
-        args.Algo_choice="Algo_choice"
+        args.Algo_choice = "Algo_choice"
     if "world_size" not in args.__dict__:
-        args.world_size=1
+        args.world_size = 1
     args.distances_unique = True
 
     # Convert dict from based on classes to based on dimension, making each dimension a class
-    features_all_classes, class_identifiers = tools.features_to_dim(features_all_classes, classes_to_process=None)
+    features_all_classes, class_identifiers = tools.features_to_dim(
+        features_all_classes, classes_to_process=None
+    )
 
-    OOD_Method = getattr(opensetAlgos, f'{args.Algo_choice}_Training')
+    OOD_Method = getattr(opensetAlgos, f"{args.Algo_choice}_Training")
     pos_classes_to_process = list(features_all_classes.keys())
     div, mod = divmod(len(pos_classes_to_process), args.world_size)
-    pos_classes_to_process = pos_classes_to_process[gpu * div + min(gpu, mod):(gpu + 1) * div + min(gpu + 1, mod)]
+    pos_classes_to_process = pos_classes_to_process[
+        gpu * div + min(gpu, mod) : (gpu + 1) * div + min(gpu + 1, mod)
+    ]
     print(f"Processing classes {pos_classes_to_process}")
 
-    algo_iterator = OOD_Method(pos_classes_to_process, features_all_classes, args, gpu, models)
+    algo_iterator = OOD_Method(
+        pos_classes_to_process, features_all_classes, args, gpu, models
+    )
     for output in algo_iterator:
         yield output
+
 
 def PDW_Inference(
     pos_classes_to_process: List[str],
     features_all_classes: Dict[str, torch.Tensor],
     args,
     gpu: int,
-    models=None
+    models=None,
 ):
     if "Algo_choice" not in args.__dict__:
-        args.Algo_choice="OpenMax"
+        args.Algo_choice = "OpenMax"
     args.distances_unique = True
 
     # Convert dict from based on classes to based on dimension, making each dimension a class
-    features_all_classes, class_identifiers = tools.features_to_dim(features_all_classes,
-                                                                    classes_to_process=pos_classes_to_process)
+    features_all_classes, class_identifiers = tools.features_to_dim(
+        features_all_classes, classes_to_process=pos_classes_to_process
+    )
 
     pos_classes_to_process = list(features_all_classes.keys())
     print(f"Processing classes {pos_classes_to_process}")
 
     # Set default shape scale of weibull model if none could be computed
-    models = opensetAlgos.heuristic.set_shape_scale_defaults(models,
-                                                             set_shape_to=args.set_shape_to,
-                                                             set_scale_to=args.set_scale_to)
+    models = opensetAlgos.heuristic.set_shape_scale_defaults(
+        models, set_shape_to=args.set_shape_to, set_scale_to=args.set_scale_to
+    )
 
-    OOD_Method = getattr(opensetAlgos, f'{args.Algo_choice}_Inference')
-    algo_iterator = OOD_Method(pos_classes_to_process, features_all_classes, args, gpu, models)
+    OOD_Method = getattr(opensetAlgos, f"{args.Algo_choice}_Inference")
+    algo_iterator = OOD_Method(
+        pos_classes_to_process, features_all_classes, args, gpu, models
+    )
     all_class_results = []
     for output in algo_iterator:
         string, (batch_to_process, probs) = output
@@ -148,5 +162,5 @@ def PDW_Inference(
         all_class_results.append(probs)
     all_class_results = torch.stack(all_class_results, dim=1)
     for cls in set(class_identifiers.tolist()):
-        probs = all_class_results[class_identifiers==cls,:]
+        probs = all_class_results[class_identifiers == cls, :]
         yield ("probs", (cls, probs))
